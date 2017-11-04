e9880f9da14739b121c4ad1ec37c149ea4e5145c
Andreas <andreashi2@gmail.com>
2017-11-05 00:40:55 +0100

    Updated to python 3

--- agent/spawn_jobs.py
+++ agent/spawn_jobs.py
@@ -1,17 +1,16 @@
 import subprocess
 import multiprocessing
 
-import signal
 
 
 def work(args):
     subprocess.call(["python", "-m" "agent.agent_runner", "False"])
 
 if __name__ == '__main__':
-    count = 20
+    count = 8
     pool = multiprocessing.Pool(processes=count)
     try:
-        results = pool.map_async(work, range(count)).get(9999999)
+        results = pool.map_async(work, range(count)).get(9999)
 
     except KeyboardInterrupt:
         pool.terminate()
--- brain/sc2_api.py
+++
@@ -1,9 +0,0 @@
-import numpy
-from pysc2.agents import base_agent
-from pysc2.lib import actions
-
-
-def random_args(action_spec_functions, function_id):
-    args = [[numpy.random.randint(0, size) for size in arg.sizes]
-            for arg in action_spec_functions[function_id].args]
-    return actions.FunctionCall(function_id, args)
